471eedc236c15361f6a66c708e4381c5a1656b6d
Jeffrey McFarland <jlmcfarlandj@gmail.com>
2021-08-12 13:59:17 -0500

    add hardcoded title area and static side nav closed frame

--- App.js
+++ App.js
@@ -1,25 +1,84 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect } from 'react';
+import { StyleSheet, Text, View, Dimensions } from 'react-native';
+
+const window = Dimensions.get("window")
 
 export default function App() {
+
+  useEffect(() => {
+    console.log('hello')
+  });
+
+  const RecipeTitle = () => {
+    return (
+      <View style={title.container}>
+        <Text style={title.title}>Tikka Masala</Text>
+      </View>
+    )
+  }
+
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Hello World</Text>
+    <View style={mainContainer.container}>
+      <View style={sideNav.container}>
+        <View style={openIcon.container}>
+          <View style={openIcon.icon}></View>
+        </View>
+      </View>
+      <RecipeTitle />
       <StatusBar style="auto" />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
+const mainContainer = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#db70a6',
-    alignItems: 'center',
-    justifyContent: 'center',
+  }
+})
+
+const sideNav = StyleSheet.create({
+  container: {
+    backgroundColor: '#ffffff',
+    alignItems: 'flex-start',
+    height: '100%',
+    width: window.width / 10,
+    zIndex: 1,
+    position: 'absolute',
+    borderRightWidth: 2,
+    borderColor: '#282828'
+  }
+})
+
+const openIcon = StyleSheet.create({
+  container: {
+    position: 'absolute',
+    top: '50%',
+    // left: '100%',
+    width: window.width / 5,
+    alignItems: 'center'
   },
-  text: {
-    color: '#ffffff',
-    fontSize: 30
+  icon: {
+    width: 35,
+    height: 35,
+    borderRadius: 35,
+    borderWidth: 2,
+    borderColor: '#282828',
+    backgroundColor: '#ffffff',
+    zIndex: 2
+  }
+})
+
+const title = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#ffffff',
+    alignItems: 'center',
+    top: 70
+  },
+  title: {
+    color: '#282828',
+    fontSize: 30,
+    fontWeight: '500'
   }
 });
--- README.md
+++ README.md
@@ -2,6 +2,4 @@
 
 ## Work In Progress
 
-##
-
 This will be a React Native App served through Expo. A passion project designed in figma first and then built with React Native through Expo since this is my first experience with Mobile development.
