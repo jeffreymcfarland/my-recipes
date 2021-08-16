ef4ab64512b4472546b1e08200922f6487fb6797
Jeffrey McFarland <jlmcfarlandj@gmail.com>
2021-08-16 16:35:59 -0500

    work out architecture for navigation and item rendering

--- App.js
+++ App.js
@@ -1,26 +1,99 @@
 import { StatusBar } from 'expo-status-bar';
-import React, { useEffect } from 'react';
-import { StyleSheet, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { StyleSheet, View, Dimensions, TouchableWithoutFeedback } from 'react-native';
 import SideNav from './components/sideNav';
 import RecipeTitle from './components/recipeTitle';
 
+const window = Dimensions.get('window')
+
 export default function App() {
-  
+  const [recipes, setRecipes] = useState([
+    {
+      'id': 1,
+      'title': 'Tikka Masala',
+      'body': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc finibus varius quam ut varius. Donec auctor eu enim quis viverra. Curabitur vitae tempus erat, a volutpat lorem. Aenean maximus justo interdum elit pharetra viverra. Nullam cursus dapibus purus eget cursus. Vivamus sed congue tellus, id convallis nunc. Nunc vehicula enim urna, eget posuere sapien lobortis a. Maecenas volutpat aliquet nisi, porta porttitor lorem vulputate placerat. Aliquam erat volutpat. Sed malesuada enim ex, et bibendum est convallis eget. Nunc rhoncus commodo eros, ut rutrum mauris. Suspendisse pharetra ut metus nec sollicitudin. Suspendisse consectetur volutpat neque. Aenean interdum condimentum porta. Praesent convallis, orci pellentesque laoreet iaculis, tellus massa tincidunt lectus, ac convallis tortor sapien sodales neque.'
+    },
+    {
+      'id': 2,
+      'title': 'Brussel Tacos',
+      'body': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc finibus varius quam ut varius. Donec auctor eu enim quis viverra. Curabitur vitae tempus erat, a volutpat lorem. Aenean maximus justo interdum elit pharetra viverra. Nullam cursus dapibus purus eget cursus. Vivamus sed congue tellus, id convallis nunc. Nunc vehicula enim urna, eget posuere sapien lobortis a. Maecenas volutpat aliquet nisi, porta porttitor lorem vulputate placerat. Aliquam erat volutpat. Sed malesuada enim ex, et bibendum est convallis eget. Nunc rhoncus commodo eros, ut rutrum mauris. Suspendisse pharetra ut metus nec sollicitudin. Suspendisse consectetur volutpat neque. Aenean interdum condimentum porta. Praesent convallis, orci pellentesque laoreet iaculis, tellus massa tincidunt lectus, ac convallis tortor sapien sodales neque.'
+    },
+    {
+      'id': 3,
+      'title': 'Kale Caesar',
+      'body': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc finibus varius quam ut varius. Donec auctor eu enim quis viverra. Curabitur vitae tempus erat, a volutpat lorem. Aenean maximus justo interdum elit pharetra viverra. Nullam cursus dapibus purus eget cursus. Vivamus sed congue tellus, id convallis nunc. Nunc vehicula enim urna, eget posuere sapien lobortis a. Maecenas volutpat aliquet nisi, porta porttitor lorem vulputate placerat. Aliquam erat volutpat. Sed malesuada enim ex, et bibendum est convallis eget. Nunc rhoncus commodo eros, ut rutrum mauris. Suspendisse pharetra ut metus nec sollicitudin. Suspendisse consectetur volutpat neque. Aenean interdum condimentum porta. Praesent convallis, orci pellentesque laoreet iaculis, tellus massa tincidunt lectus, ac convallis tortor sapien sodales neque.'
+    },
+    {
+      'id': 4,
+      'title': 'Gorgonzola Red Sauce Pasta',
+      'body': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc finibus varius quam ut varius. Donec auctor eu enim quis viverra. Curabitur vitae tempus erat, a volutpat lorem. Aenean maximus justo interdum elit pharetra viverra. Nullam cursus dapibus purus eget cursus. Vivamus sed congue tellus, id convallis nunc. Nunc vehicula enim urna, eget posuere sapien lobortis a. Maecenas volutpat aliquet nisi, porta porttitor lorem vulputate placerat. Aliquam erat volutpat. Sed malesuada enim ex, et bibendum est convallis eget. Nunc rhoncus commodo eros, ut rutrum mauris. Suspendisse pharetra ut metus nec sollicitudin. Suspendisse consectetur volutpat neque. Aenean interdum condimentum porta. Praesent convallis, orci pellentesque laoreet iaculis, tellus massa tincidunt lectus, ac convallis tortor sapien sodales neque.'
+    }
+  ])
+
+  const [navOpen, setNavOpen] = useState(false)
+  const [iconRotate, setIconRotate] = useState('0deg')
+  const [navContainerSize, setNavContainerSize] = useState(16)
+  const [iconPosition, setIconPosition] = useState(8)
+  const [recipeTitle, setRecipeTitle] = useState(recipes[0].title)
+
   useEffect(() => {
-    console.log('hello')
+    
   });
 
+  const openNav = () => {
+    setIconRotate('180deg')
+    setNavContainerSize(2)
+    setIconPosition(1)
+    setNavOpen(true)
+  }
+
+  const closeNav = () => {
+    setIconRotate('0deg')
+    setNavContainerSize(16)
+    setIconPosition(8)
+    setNavOpen(false)
+  }
+
+  const toggleNav = () => {
+    if (!navOpen) {
+      openNav()
+    } else {
+      closeNav()
+    }
+  }
+
+  const selectRecipe = (title) => {
+    if(title != recipeTitle) {
+      setRecipeTitle(title)
+    }
+  }
+
   return (
-    <View style={mainContainer.container}>
-      <SideNav />
-      <RecipeTitle />
-      <StatusBar style="auto" />
-    </View>
+    <TouchableWithoutFeedback style={mainContainer.wrapper} onPress={closeNav}>
+      <View style={mainContainer.container}>
+        <SideNav
+          toggleNav={toggleNav}
+          navOpen={navOpen}
+          iconRotate={iconRotate}
+          navContainerSize={navContainerSize}
+          iconPosition={iconPosition}
+          window={window}
+          recipes={recipes}
+          selectRecipe={selectRecipe}
+        />
+        <RecipeTitle title={recipeTitle} />
+        <StatusBar style="auto" />
+      </View>
+    </TouchableWithoutFeedback>
   );
 }
 
 const mainContainer = StyleSheet.create({
   container: {
     flex: 1
+  },
+  wrapper: {
+    height: window.height,
+    zIndex: 0
   }
 })
---
+++ components/navItems.js
@@ -0,0 +1,71 @@
+import React from 'react';
+import { 
+  StyleSheet, 
+  View, 
+  Text,
+  FlatList,
+  TouchableHighlight } from 'react-native';
+
+const Item = ({ title, selectRecipe }) => (
+  <TouchableHighlight
+    style={navItems.touch}
+    onPress={() => selectRecipe(title)}
+    underlayColor={'#F5F5F5'}
+  >
+    <View style={navItems.itemsWrapper}>
+      <Text style={navItems.item} numberOfLines={1}>{title}</Text>
+    </View>
+  </TouchableHighlight>
+)
+
+export default function SideNav({ navOpen, recipes, selectRecipe }) {
+
+  return(
+    <>
+      {navOpen ?
+      <View style={navItems.container}>
+        <FlatList 
+          data={recipes}
+          renderItem={({ item }) => <Item title={item.title} selectRecipe={selectRecipe} />}
+          keyExtractor={item => item.id.toString()}
+          style={navItems.list}
+        />
+      </View>
+      :
+      <></>
+      }
+    </>
+  )
+}
+
+const navItems = StyleSheet.create({
+  container: {
+    width: '100%',
+    height: '100%',
+    alignItems: 'center',
+    justifyContent: 'center',
+    zIndex: 4
+  },
+  list: {
+    width: '100%',
+    paddingTop: 60,
+    zIndex: 5
+  },
+  touch: {
+    width: '100%',
+    
+  },
+  itemsWrapper: {
+    width: '100%',
+    height: 60,
+    alignItems: 'flex-start',
+    justifyContent: 'center',
+    zIndex: 6
+  },
+  item: {
+    marginLeft: 20,
+    height: 'auto',
+    fontSize: 18,
+    fontWeight: '600'
+  }
+})
